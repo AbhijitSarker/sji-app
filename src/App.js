d56ddbf8aad0ea765724ee0af94cc65a9b7a38ff
AbhijitSarker <abhijitsarker03@gmail.com>
2023-10-27 14:29:34 +0600

    folder sturcture updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <h1>hello SJ Innovation</h1>
     </div>
   );
